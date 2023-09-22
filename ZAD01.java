ba6ef2edc256aab43425affa7f445af815d5642b
petarmudev <139475634+petarmudev@users.noreply.github.com>
2023-09-22 15:23:57 +0300

    I don`t know what I did...

--- ZAD01.java
+++ ZAD01.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class ZAD01 {
     public static void main(String[] args) {
-        Scanner scanner =
+        Scanner scanner = new Scanner(System.in);
+        int num1 = Integer.parseInt(scanner.nextLine());
+        Scanner scanner2 = new Scanner(System.in);
+        double asd = Double.parseDouble(scanner.nextLine());
+        double das = Double.parseDouble(scanner.nextLine());
+
     }
 }

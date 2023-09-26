7d2e2c0e93cb8a8e6b9289d028213dc7f050ea4e
petarmudev <139475634+petarmudev@users.noreply.github.com>
2023-09-26 11:24:57 +0300

    so far, switching to the laptop

---
+++ Projects/Java Fundamentals/src/L04_ExerciseDataTypesAndVariables/Zad08_BeerKegs.java
@@ -0,0 +1,32 @@
+package L04_ExerciseDataTypesAndVariables;
+
+import java.util.Scanner;
+
+public class Zad08_BeerKegs {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //
+        int countKegs = Integer.parseInt(scanner.nextLine());
+        //за всеки кег от 1 до последния ( countKegs):
+        //1 model -> String
+        // radius -> double
+        //height -> integer
+        //obem (Volume) -> PI * r * r * height
+        double maxVolume = Double.MIN_VALUE;
+        String maxModel = "";
+        for (int keg = 1; keg <= countKegs ; keg++) {
+            String model = scanner.nextLine();
+            double radius = Double.parseDouble(scanner.nextLine());
+            int height = Integer.parseInt(scanner.nextLine());
+
+            double volume = Math.PI * Math.pow(radius, 2) * height;
+
+            if (volume > maxVolume) {
+                maxVolume = volume;
+                maxModel = model;
+            }
+        }
+        System.out.println(maxModel);
+
+    }
+}
---
+++ Projects/Java Fundamentals/src/L04_ExerciseDataTypesAndVariables/Zad10_PokeMon.java
@@ -0,0 +1,10 @@
+package L04_ExerciseDataTypesAndVariables;
+
+import java.util.Scanner;
+
+public class Zad10_PokeMon {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}

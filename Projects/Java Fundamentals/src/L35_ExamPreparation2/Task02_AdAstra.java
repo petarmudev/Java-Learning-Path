a456f5b977324ace08d7726d8d395cd3f1f08906
petarmudev <139475634+petarmudev@users.noreply.github.com>
2023-11-27 16:07:26 +0200

    Final exam preparation 2

---
+++ Projects/Java Fundamentals/src/L35_ExamPreparation2/Task02_AdAstra.java
@@ -0,0 +1,33 @@
+package L35_ExamPreparation2;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task02_AdAstra {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String foodInfo = scanner.nextLine();
+        String regex = "(#|\\|)(?<item>[A-Za-z]+\\s?[A-Za-z]+)\\1(?<date>[0-9]{2}/[0-9]{2}/[0-9]{2})\\1(?<calories>[0-9]+)\\1";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(foodInfo);
+
+        int totalCaloriesSum = getTotalCalories(pattern, foodInfo);
+        System.out.printf("You have food to last you for: %d days!%n", totalCaloriesSum / 2000);
+
+        while (matcher.find()) {
+            System.out.printf("Item: %s, Best before: %s, Nutrition: %s%n", matcher.group("item"), matcher.group("date"), matcher.group("calories"));
+        }
+
+    }
+
+    private static int getTotalCalories(Pattern pattern, String foodInfo) {
+        Matcher matcher = pattern.matcher(foodInfo);
+        int totalCaloriesSum = 0;
+        while (matcher.find()) {
+            totalCaloriesSum += Integer.parseInt(matcher.group("calories"));
+        }
+        return totalCaloriesSum;
+    }
+}

abe950f70b0423c5e0870473cde38d11c257b683
petarmudev <139475634+petarmudev@users.noreply.github.com>
2023-12-01 18:55:03 +0200

    Self preparation
    Fixed Task01. Had to use the original input and change itself

--- Projects/Java Fundamentals/src/Fund_Exams_04/ProgrammingFundamentalsFinalExam/Task01_PasswordReset.java
+++ Projects/Java Fundamentals/src/Fund_Exams_04/ProgrammingFundamentalsFinalExam/Task01_PasswordReset.java
@@ -6,8 +6,7 @@ public class Task01_PasswordReset {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String text = scanner.nextLine();
-        String newPassword = "";
+        String password = scanner.nextLine();
 
         while (true) {
             String input = scanner.nextLine();
@@ -19,41 +18,42 @@ public class Task01_PasswordReset {
 
             switch (command) {
                 case "TakeOdd":   //Takes only the characters at odd indices and concatenates them to obtain the new raw password and then prints it.
-
-                    for (int i = 1; i < text.length(); i += 2) {
-                        newPassword += text.charAt(i);
+                    StringBuilder newTempPass = new StringBuilder();
+                    for (int i = 1; i < password.length(); i++) {
+                        if (i % 2 != 0) {
+                            newTempPass.append(password.charAt(i));
+                        }
                     }
-                    System.out.println(newPassword);
+                    password = newTempPass.toString();
+                    System.out.println(password);
                     break;
 
                 case "Cut":     //"Cut {index} {length}"
                     int index = Integer.parseInt(commandParts[1]);
-                    int lenght = Integer.parseInt(commandParts[2]);
+                    int lenght = index + Integer.parseInt(commandParts[2]);
 
-                    String substring = newPassword.substring(0, index);
-                    String substringToConcat = newPassword.substring((index + lenght));
+//                    String substring = newPassword.substring(0, index);
+//                    String substringToConcat = newPassword.substring((lenght));
 
-                    newPassword = substring + substringToConcat;
-                    System.out.println(newPassword);
+                    password = new StringBuilder(password).delete(index, lenght).toString();
+                    System.out.println(password);
                     break;
 
                 case "Substitute":      //"Substitute {substring} {substitute}"
                     // "Substitute :: -"
-                    String replace = commandParts[1];
+                    String target = commandParts[1];
                     String replacement = commandParts[2];
 
-                    if (newPassword.contains(replace)) {
-                        newPassword = newPassword.replaceAll(replace, replacement);
-                        System.out.println(newPassword);
+                    if (password.contains(target)) {
+                        password = password.replaceAll(target, replacement);
+                        System.out.println(password);
                     } else {
                         System.out.println("Nothing to replace!");
                     }
                     break;
-                default:
-                    break;
             }
         }
         //Print: "Your password is: {password}"
-        System.out.printf("Your password is: %s", newPassword);
+        System.out.println("Your password is: " + password);
     }
 }
---
+++ Projects/Java Fundamentals/src/Fund_Exams_04/ProgrammingFundamentalsFinalExam/Task02_FancyBarcodes.java
@@ -0,0 +1,10 @@
+package Fund_Exams_04.ProgrammingFundamentalsFinalExam;
+
+import java.util.Scanner;
+
+public class Task02_FancyBarcodes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}

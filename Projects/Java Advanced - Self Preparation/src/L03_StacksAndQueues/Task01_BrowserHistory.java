1f80c167146c1a1656fb1b9778439098bf6c0f7f
petarmudev <139475634+petarmudev@users.noreply.github.com>
2023-12-04 19:48:50 +0200

    Java Advanced - Self Preparation

---
+++ Projects/Java Advanced - Self Preparation/src/L03_StacksAndQueues/Task01_BrowserHistory.java
@@ -0,0 +1,35 @@
+package L03_StacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class Task01_BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayDeque<String> history = new ArrayDeque<>();
+
+        String defaultMsg = "no previous URLs";
+        String current = "";
+        while (true) {
+            String input = scanner.nextLine();
+            if ("Home".equals(input)) {
+                break;
+            }
+            if (input.equals("back")) {
+                if (history.size() == 1) {
+                    System.out.println(defaultMsg);
+                } else {
+                    history.pop();
+                    if (history.isEmpty()) {
+                        System.out.println(defaultMsg);
+                    } else {
+                        System.out.println(history.peek());
+                    }
+                }
+            } else {
+                history.push(input);
+                System.out.println(input);
+            }
+        }
+    }
+}

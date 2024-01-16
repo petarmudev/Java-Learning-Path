134f2bc823b927556c03b10dd3eaed665aedc15f
petarmudev <139475634+petarmudev@users.noreply.github.com>
2024-01-16 20:49:13 +0200

    so far

--- Projects/Java Advanced - Self-Preparation/src/L09_StreamsFilesAndDirectories/Task05_WriteEveryThirdLine.java
+++ Projects/Java Advanced - Self-Preparation/src/L09_StreamsFilesAndDirectories/Task05_WriteEveryThirdLine.java
@@ -1,15 +1,29 @@
 package L09_StreamsFilesAndDirectories;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class Task05_WriteEveryThirdLine {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
 
         String pathIn = "C:\\Users\\Petar\\Documents\\GitHub\\Java-Learning-Path\\Projects\\Java Advanced - Self-Preparation\\src\\L09_StreamsFilesAndDirectories\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
         String pathOut = "C:\\Users\\Petar\\Documents\\GitHub\\Java-Learning-Path\\Projects\\Java Advanced - Self-Preparation\\src\\L09_StreamsFilesAndDirectories\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\05.WriteEveryThirdLineOutput.txt";
 
 
-        
+        Scanner in = new Scanner(new FileReader(pathIn));
+        PrintWriter out = new PrintWriter(new FileWriter(pathOut));
+
+        int counter = 1;
+        String line = in.nextLine();
+        while (in.hasNextLine()) {
+            if (counter % 3 == 0) {
+                out.println(line);
+            }
+            counter++;
+            line = in.nextLine();
+        }
+        out.close();
+
     }
 }
---
+++ Projects/Java Advanced - Self-Preparation/src/L09_StreamsFilesAndDirectories/Task06_SortLines.java
@@ -0,0 +1,15 @@
+package L09_StreamsFilesAndDirectories;
+
+import java.util.Scanner;
+
+public class Task06_SortLines {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String pathIn = "C:\\Users\\Petar\\Documents\\GitHub\\Java-Learning-Path\\Projects\\Java Advanced - Self-Preparation\\src\\L09_StreamsFilesAndDirectories\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
+        String pathOut ="C:\\Users\\Petar\\Documents\\GitHub\\Java-Learning-Path\\Projects\\Java Advanced - Self-Preparation\\src\\L09_StreamsFilesAndDirectories\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\06.SortLinesOutput.txt";
+
+        
+
+    }
+}

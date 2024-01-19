ab9e88ed4923fa35f600311ce3e11107b73e6b1c
petarmudev <139475634+petarmudev@users.noreply.github.com>
2024-01-19 06:02:22 +0200

    Update Task08_NestedFolders.java

--- Projects/Java Advanced - Self-Preparation/src/L09_StreamsFilesAndDirectories/Task08_NestedFolders.java
+++ Projects/Java Advanced - Self-Preparation/src/L09_StreamsFilesAndDirectories/Task08_NestedFolders.java
@@ -7,22 +7,20 @@ import java.util.Scanner;
 
 public class Task08_NestedFolders {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        //todo the rest of the task
         String path = "C:\\Users\\Petar\\Documents\\GitHub\\Java-Learning-Path\\Projects\\Java Advanced - Self-Preparation\\src\\L09_StreamsFilesAndDirectories\\Root Files";
         //String path = "C:\\SoftUni\\Files-and-Streams";
 
-        File root = new File(path);
+        File rootDirectory = new File(path);
 
         Deque<File> dirs = new ArrayDeque<>();
-        dirs.offer(root);
+        dirs.offer(rootDirectory);
 
         int count = 0;
 
         while (!dirs.isEmpty()) {
-            File current = dirs.poll();
-            File [] nestedFiles = current.listFiles();
+            File currentDirectory = dirs.poll();
+            File [] nestedFiles = currentDirectory.listFiles();
             for (File nestedFile : nestedFiles) {
                 if (nestedFile.isDirectory()) {
                     dirs.offer(nestedFile);

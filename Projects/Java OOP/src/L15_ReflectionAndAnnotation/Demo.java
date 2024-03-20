e14f3805fbd7dfa84dec675e8282a3406725082d
petarmudev <139475634+petarmudev@users.noreply.github.com>
2024-03-20 10:43:48 +0200

    Reflection And Annotation

---
+++ Projects/Java OOP/src/L15_ReflectionAndAnnotation/Demo.java
@@ -0,0 +1,10 @@
+package L15_ReflectionAndAnnotation;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        //todo finish Lombok setup!
+
+    }
+
+}
---
+++ Projects/Java OOP/src/L15_ReflectionAndAnnotation/DemoCat.java
@@ -0,0 +1,14 @@
+package L15_ReflectionAndAnnotation;
+
+public class DemoCat {
+    //todo
+    //Lombok
+    //@Getter
+    //@Setter
+    private String name;
+    private String lastName;
+    private String owner;
+    private int age;
+
+
+}
